3fed2d0db346b07e49d04cd7f6bbfbd95a57c41b
Allard <AllardQuek@users.noreply.github.com>
2021-05-29 03:15:13 +0800

    upload to s3 onChange

--- src/App.js
+++ src/App.js
@@ -1,16 +1,76 @@
+import { useState } from "react";
 import './App.css';
 import { Button } from '@material-ui/core';
 import styled from 'styled-components';
 import CloudUploadIcon from '@material-ui/icons/CloudUpload';
 import { useForm } from "react-hook-form";
+import FileUploader from './components/FileUploader';
 
 
 function App() {
+  const axios = require('axios').default;
   const { register, handleSubmit } = useForm();
+  const [selectedAudio, setSelectedAudio] = useState(null);
+  const API_ENDPOINT = 'https://nf4yot096j.execute-api.ap-southeast-1.amazonaws.com/default/getPresignedURL';
+  const API_VID_ENDPOINT = 'https://5n58vjjzdd.execute-api.ap-southeast-1.amazonaws.com/default/getVidPresignedURL';
 
   const submit = (data) => {
-    console.log("submitting!");
     console.log(data);
+    fetch('https://8c3vifq9mj.execute-api.ap-southeast-1.amazonaws.com/default/test-socket', {
+      method: 'POST',
+      mode: 'no-cors',  // TODO: Check what the proper method is; need to handle error
+      headers: {
+        "Access-Control-Allow-Origin": '*',
+        'Content-Type': 'application/json',
+        'Accept': '*/*',
+      },
+    }).then(res => res.json())
+      .then(res => console.log(res))
+      .catch( (error) => {
+        console.log(error);
+      });
+  }
+  
+  const uploadAudio = async (file) => {
+    setSelectedAudio(file);
+    console.log("AUDIO CHANGING");
+    const response = await axios({
+      method: 'GET',
+      url: API_ENDPOINT
+    })
+
+    console.log(response.data.uploadURL);
+    console.log(file);
+
+    const result = await fetch(response.data.uploadURL, {
+      method: 'PUT',
+      headers: {
+        "Content-Type": "audio/*"
+      },
+      body: file,
+    })
+
+    console.log('Result: ', result)
+  }
+
+
+  const uploadVideo = async (file) => {
+    // * Get pre-signed URL
+    const response = await axios({
+      method: 'GET',
+      url: API_VID_ENDPOINT
+    })
+
+    // * Upload file to S3 via pre-signed URL
+    const result = await fetch(response.data.uploadURL, {
+      method: 'PUT',
+      headers: {
+        "Content-Type": "video/mp4"
+      },
+      body: file,
+    })
+
+    console.log('Result: ', result)
   }
 
   return (
@@ -21,22 +81,17 @@ function App() {
         <h1>Welcome to AnyTutor</h1>
 
         <form onSubmit={handleSubmit(submit)} className="uploads">
-
-          <input 
-            name="audio"
+          <FileUploader
+            onFileSelectSuccess={(file) => uploadAudio(file)}
+            onFileSelectError={({ error }) => alert(error)}
             accept="audio/*"
-            id="contained-button-file"
-            type="file"
-            required
-            {...register("audio-upload")} />
+          />
 
-          <input 
-            name="video"
-            accept="video/*"
-            id="contained-button-file"
-            type="file"
-            required
-            {...register("video-upload")} />
+          <FileUploader
+            onFileSelectSuccess={(file) => uploadVideo(file)}
+            onFileSelectError={({ error }) => alert(error)}
+            accept="video/mp4"
+          />
 
           <Button
             className="submit"
@@ -45,6 +100,7 @@ function App() {
             startIcon={<CloudUploadIcon />}
             type="submit"
           >Submit</Button>
+
         </form>
       </AppStyled>
     </div>
@@ -54,7 +110,10 @@ function App() {
 const AppStyled = styled.main`
   .uploads {
     display: flex;
+    justify-content: center;
     flex-direction: column;
+    align-items: center;
+    text-align: center;
   }
 
   .audio {
