620c64ee5569081db66b046c37bc63251727181c
Allard <AllardQuek@users.noreply.github.com>
2021-06-09 13:49:16 +0800

    added Home and About pages

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import "../App.css";
+import Videos from "../components/Videos";
+import Landing from "../components/Landing";
+
+const Home = () => {
+  return (
+    <>
+      <Landing />
+      <Videos />
+    </>
+  );
+};
+
+export default Home;
