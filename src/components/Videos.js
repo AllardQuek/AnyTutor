a617dd89c4c06db2fa2befb24a883ba8b1b32af2
Allard <AllardQuek@users.noreply.github.com>
2021-06-09 17:10:25 +0800

    3 videos in a row

--- src/components/Videos.js
+++ src/components/Videos.js
@@ -7,12 +7,6 @@ const useStyles = makeStyles(() => ({
     marginTop: "1rem",
     position: "relative",
     paddingBottom: "3rem",
-    display: "flex",
-  },
-  player: {
-    position: "absolute",
-    top: 0,
-    left: 0,
   },
   header: {
     color: "#fff",
@@ -30,23 +24,14 @@ function Cards() {
   return (
     <>
       <h1 className={classes.header}>Check out these sample videos!</h1>
-      <Grid container spacing={2}>
-        <Grid item xs={12}>
-          <Grid container justify="center" spacing={2}>
-            {urls.map((url, value) => (
-              <Grid key={value} item>
-                <div className={classes.video}>
-                  <ReactPlayer
-                    className="classes.player"
-                    url={url}
-                    width="100%"
-                    height="100%"
-                  />
-                </div>
-              </Grid>
-            ))}
+      <Grid container direction="row" justify="center" spacing={2}>
+        {urls.map((url, value) => (
+          <Grid key={value} item>
+            <div className={classes.video}>
+              <ReactPlayer url={url} pip={true} controls={true} width="100%" />
+            </div>
           </Grid>
-        </Grid>
+        ))}
       </Grid>
     </>
   );
