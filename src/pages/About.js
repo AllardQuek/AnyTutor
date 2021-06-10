18e64086992ff8d2e72b9f74c624a6f46e95dbed
Allard <AllardQuek@users.noreply.github.com>
2021-06-10 16:22:18 +0800

    organised pages and applied alternative bg

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,6 +1,9 @@
+import BackgroundStyle from "../styles/BackgroundStyle";
+
 const About = () => {
   return (
     <div>
+      <BackgroundStyle />
       <h1>Hi. We are Team Time Quack!</h1>
     </div>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,16 @@
-import "../App.css";
 import Videos from "../components/Videos";
 import Landing from "../components/Landing";
+import styled from "styled-components";
 
 const Home = () => {
   return (
-    <>
+    <HomeStyled>
       <Landing />
       <Videos />
-    </>
+    </HomeStyled>
   );
 };
+
+const HomeStyled = styled.div``;
 
 export default Home;
