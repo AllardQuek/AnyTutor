f230f11aa10c9a98a93f861e6fe95ced09a18ba5
Allard <AllardQuek@users.noreply.github.com>
2021-06-10 16:22:57 +0800

    remove bg video

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,12 +1,10 @@
 import "../App.css";
 import "./Landing.css";
 import { Button } from "@material-ui/core";
-import homeVid from "../videos/homeVid.mp4";
 
 function Landing() {
   return (
     <div className="landing-container">
-      <video src={homeVid} autoPlay loop muted />
       <h1>REINVENT EDUCATION</h1>
       <p>What are you waiting for?</p>
       <Button
