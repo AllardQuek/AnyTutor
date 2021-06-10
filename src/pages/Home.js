16de9fb20b0ef03dc84dcc3687f8b18896c3c848
Allard <AllardQuek@users.noreply.github.com>
2021-06-11 02:58:42 +0800

    added custom navbar style

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import Videos from "../components/Videos";
 import Landing from "../components/Landing";
 import styled from "styled-components";
+import NavbarStyle from "../styles/NavbarStyle";
 
 const Home = () => {
   return (
     <HomeStyled>
+      <NavbarStyle />
       <Landing />
       <Videos />
     </HomeStyled>
