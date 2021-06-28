364df692838238db48144d3c94a9a7f8a639b30d
Allard <AllardQuek@users.noreply.github.com>
2021-06-28 13:06:23 +0800

    disabled submit button when submitting

--- src/pages/Upload.js
+++ src/pages/Upload.js
@@ -11,8 +11,12 @@ const Upload = ({ text, mediaType, uploadEmail, lessonVid }) => {
   const { handleSubmit } = useForm();
   const [firstUpload, setFirstUpload] = useState(false);
   const [secondUpload, setSecondUpload] = useState(false);
+  const [submitting, setSubmitting] = useState(false);
+  console.log(submitting);
 
   const submit = (data) => {
+    setSubmitting(true);
+
     // * Endpoint to lambda that will run our notebook
     const url =
       "https://8c3vifq9mj.execute-api.ap-southeast-1.amazonaws.com/default/test-socket";
@@ -34,11 +38,12 @@ const Upload = ({ text, mediaType, uploadEmail, lessonVid }) => {
     })
       .then((res) => res.json())
       .then((res) => console.log(res))
-      .then((res) =>
+      .then((res) => {
+        setSubmitting(false);
         alert(
           "Submitted! You will receive the result in your email in about 5 minutes (for a ~20s video) :)"
-        )
-      )
+        );
+      })
       .catch((error) => {
         console.log("CAUGHT ERROR");
         console.log(error);
@@ -88,7 +93,7 @@ const Upload = ({ text, mediaType, uploadEmail, lessonVid }) => {
 
       <form onSubmit={handleSubmit(submit)} className="uploads">
         {/* If both uploads have been made enable submit button */}
-        {firstUpload && secondUpload ? (
+        {(firstUpload && secondUpload) || !submitting ? (
           <CustomButton text="Submit" className="btn-submit" />
         ) : (
           <DisabledButton text="Submit" className="btn-submit" />
